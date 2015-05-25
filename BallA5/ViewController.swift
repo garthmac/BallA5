da251246121ba72ee496e8142519288205655001
Garth MacKenzie <garthmac@juno.com>
2015-05-24 19:12:57 -0600

    Initial Commit

---
+++ BallA5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BallA5
+//
+//  Created by iMac21.5 on 5/24/15.
+//  Copyright (c) 2015 Garth MacKenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
